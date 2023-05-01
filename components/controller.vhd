840e5e5d2466427add4669ae7d949261221dc569
Ard Geuze <geuzeard@gmail.com>
2023-05-01 13:09:06 +0200

    V1 of checkpoints

--- components/controller.vhd
+++ components/controller.vhd
@@ -6,7 +6,9 @@ entity controller is
         clk   : in std_logic;
         reset : in std_logic;
 
-        sensor_data : in std_logic_vector (2 downto 0);
+        sensor_data     : in std_logic_vector (2 downto 0);
+        next_checkpoint : in std_logic_vector (1 downto 0); -- 00 = straight, 01 = left, 10 = right, 11 = backwards
+        stop_checkpoint : in std_logic;
 
         motor_l_reset     : out std_logic;
         motor_l_direction : out std_logic;
@@ -18,8 +20,9 @@ end entity controller;
 
 architecture behavioural of controller is
 
-    signal motor_left_reset, motor_right_reset : std_logic := '0';
+    signal motor_left_reset, motor_right_reset         : std_logic := '0';
     signal motor_left_direction, motor_right_direction : std_logic := '0';
+    signal turning, double_turning                     : std_logic := '0';
 
 begin
 
@@ -29,42 +32,79 @@ begin
             if (reset = '1') then
                 motor_left_reset  <= '1';
                 motor_right_reset <= '1';
+                turning           <= '0';
+                double_turning    <= '0';
+            elsif (double_turning = '1') then
+                if (sensor_data = "100") then
+                    turning        <= '1';
+                    double_turning <= '0';
+                end if;
+            elsif (turning = '1') then
+                if (sensor_data = "011" or sensor_data = "110") then
+                    turning <= '0';
+                end if;
             elsif (sensor_data = "000") then
+                -- Checkpoint
+                if (stop_checkpoint = '1') then
+                    motor_left_reset  <= '1';
+                    motor_right_reset <= '1';
+                elsif (next_checkpoint = "00") then
+                    -- Straight
+                    motor_left_reset      <= '0';
+                    motor_right_reset     <= '0';
+                    motor_left_direction  <= '1';
+                    motor_right_direction <= '0';
+                elsif (next_checkpoint = "01") then
+                    -- Left
+                    motor_left_reset      <= '1';
+                    motor_right_reset     <= '0';
+                    motor_right_direction <= '0';
+                    turning               <= '1';
+                elsif (next_checkpoint = "10") then
+                    -- Right
+                    motor_left_reset     <= '0';
+                    motor_right_reset    <= '1';
+                    motor_left_direction <= '1';
+                    turning              <= '1';
+                elsif (next_checkpoint = "11") then
+                    -- Backwards
+                    motor_left_reset      <= '0';
+                    motor_right_reset     <= '0';
+                    motor_left_direction  <= '0';
+                    motor_right_direction <= '0';
+                    double_turning        <= '1';
+                end if;
+            elsif (sensor_data = "001") then
+                motor_left_reset      <= '1';
+                motor_right_reset     <= '0';
+                motor_right_direction <= '0';
+            elsif (sensor_data = "010") then
                 motor_left_reset      <= '0';
                 motor_right_reset     <= '0';
                 motor_left_direction  <= '1';
                 motor_right_direction <= '0';
-            elsif (sensor_data = "001") then
-                motor_left_reset  <= '1';
-                motor_right_reset <= '0';
-                motor_right_direction <= '0';
-            elsif (sensor_data = "010") then
-                motor_left_reset  <= '0';
-                motor_right_reset <= '0';
-                motor_left_direction  <= '1';
-                motor_right_direction <= '0';
             elsif (sensor_data = "011") then
-                motor_left_reset  <= '0';
-                motor_right_reset <= '0';
+                motor_left_reset      <= '0';
+                motor_right_reset     <= '0';
                 motor_left_direction  <= '0';
                 motor_right_direction <= '0';
             elsif (sensor_data = "100") then
-                motor_left_reset  <= '0';
-                motor_right_reset <= '1';
-                motor_left_direction  <= '1';
+                motor_left_reset     <= '0';
+                motor_right_reset    <= '1';
+                motor_left_direction <= '1';
             elsif (sensor_data = "101") then
-                motor_left_reset  <= '0';
-                motor_right_reset <= '0';
+                motor_left_reset      <= '0';
+                motor_right_reset     <= '0';
                 motor_left_direction  <= '1';
                 motor_right_direction <= '0';
             elsif (sensor_data = "110") then
-                motor_left_reset  <= '0';
-                motor_right_reset <= '0';
+                motor_left_reset      <= '0';
+                motor_right_reset     <= '0';
                 motor_left_direction  <= '1';
                 motor_right_direction <= '1';
             elsif (sensor_data = "111") then
-                motor_left_reset  <= '0';
-                motor_right_reset <= '0';
+                motor_left_reset      <= '0';
+                motor_right_reset     <= '0';
                 motor_left_direction  <= '1';
                 motor_right_direction <= '0';
             end if;
