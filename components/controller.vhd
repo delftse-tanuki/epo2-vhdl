d8a23748ac89efd81b9da320b15f32e5e7941d84
sfakhir <sami@fakhir.nl>
2023-06-14 15:12:24 +0200

    process 2 clk sensitivity list

--- components/controller.vhd
+++ components/controller.vhd
@@ -9,8 +9,6 @@ entity controller is
         sensor_data    : in std_logic_vector (2 downto 0);
         next_direction : in std_logic_vector (1 downto 0); -- 00 = straight, 01 = left, 10 = right, 11 = start/stop
 
-        mine_detected     : in std_logic;
-
         motor_l_reset     : out std_logic;
         motor_l_direction : out std_logic; -- 1 = forward, 0 = backwards
 
@@ -23,10 +21,11 @@ end entity controller;
 
 architecture behavioural of controller is
 
-    signal motor_left_reset, motor_right_reset, drive  : std_logic;
+    signal motor_left_reset, motor_right_reset  : std_logic;
     signal motor_left_direction, motor_right_direction : std_logic;
     signal skip_checkpoint, checkpoint, backwards      : std_logic;
     signal turning, skip_turn                          : std_logic;
+    signal drive                                       : std_logic
 
 begin
 
@@ -34,30 +33,22 @@ begin
     begin
         if (rising_edge(clk)) then
             if (reset = '1') then
-                motor_left_reset   <= '1';
-                motor_right_reset  <= '1';
-                skip_checkpoint    <= '0';
-                checkpoint         <= '0';
-                drive              <= '0';
-                backwards          <= '0';
-                turning            <= '0';
-                skip_turn          <= '0';
-                ask_next_direction <= '0';
+                motor_left_reset  <= '1';
+                motor_right_reset <= '1';
+                skip_checkpoint   <= '1';
+                checkpoint        <= '0';
+                drive             <= '0';
+                backwards         <= '0';
+                turning           <= '0';
+                skip_turn         <= '0';
             elsif (drive = '0') then
                 motor_left_reset  <= '1';
                 motor_right_reset <= '1';
                 if (next_direction = "11") then
-                    drive              <= '1';
-                    ask_next_direction <= '1';
+                    drive           <= '1';
+                    skip_checkpoint <= '0';
                 else null;
                 end if;
-            elsif (mine_detected = '1') then
-                motor_left_reset      <= '0';
-                motor_right_reset     <= '0';
-                motor_left_direction  <= '0';
-                motor_right_direction <= '0';
-                turning               <= '1';
-                skip_turn             <= '1';
             elsif (backwards = '1') then
                 motor_left_reset      <= '0';
                 motor_right_reset     <= '0';
@@ -151,15 +142,9 @@ begin
                 end if;
             else null;
             end if;
-            if (checkpoint = '1' and sensor_data = "101") then
+            if (checkpoint = '1' and sensor_data = "101" and drive = '1') then
                 checkpoint <= '0';
-                if (skip_checkpoint = '1') then
-                    skip_checkpoint    <= '0';
-                    ask_next_direction <= '1';
-                else
-                    skip_checkpoint    <= '1';
-                    ask_next_direction <= '0';
-                end if;
+                skip_checkpoint <= not skip_checkpoint;
             else null;
             end if;
         else null;
@@ -171,5 +156,7 @@ begin
 
     motor_l_direction <= motor_left_direction;
     motor_r_direction <= motor_right_direction;
+
+    ask_next_direction <= not skip_checkpoint;
 
 end architecture behavioural;
